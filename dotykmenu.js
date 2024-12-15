dfa2b824a415a33997316deafee8c230b80d0f88
MateuszSerkowski <116501028+MateuszSerkowski@users.noreply.github.com>
2024-12-16 00:14:54 +0100

    Update dotykmenu.js
    cooo

--- dotykmenu.js
+++ dotykmenu.js
@@ -1,1 +1,8 @@
-nic
+const images = document.querySelectorAll('.category-items img');
+
+images.forEach(image => {
+    image.addEventListener('click', function() {
+        // Dodajemy klasę "active" do obrazu, aby aktywować efekty
+        image.classList.toggle('active');
+    });
+});
